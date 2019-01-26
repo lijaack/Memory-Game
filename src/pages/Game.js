4951c8c1dc1e089eda7fb485e6fc8a9480a442ec
Jack <lijaack@yahoo.com>
2019-01-26 01:51:47 -0800

    changed to setState

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -7,24 +7,37 @@ import CartoonCard from "../components/CartoonCard";
 
 class Game extends Component {
     state = {
-        cartoons: [],
+        cartoons: [] ,
         score: 0,
         clicked: {}
       };
     
-
+    componentDidMount() {
+        this.setState({cartoons: Cartoon})
+        console.log(this.state.cartoons)
+    }
+    
     clickedImage = event => {
         console.log(event.target.id)
         if(this.state.clicked[event.target.id]){
             console.log("you lose")
-            this.state.clicked={};
-            this.state.score=0;
+            this.setState({clicked: {}});
+            this.setState({score: 0});
         } else {
-            this.state.clicked[event.target.id]=1;
-            this.state.score ++
+            this.setState({clicked: {...this.state.clicked, [event.target.id]:1}});
+            this.setState({score: this.state.score + 1});
             console.log(this.state.score)
+            this.shuffleCartoons()
         }
     }
+
+    shuffleCartoons = () => {
+        for(var i=0; i < this.state.cartoons.length; i++){
+            var random = Math.ceil(Math.random(this.state.cartoons.length))
+            console.log(random)
+        }
+    }
+
     render(){
         return(
             <div>
@@ -34,7 +47,7 @@ class Game extends Component {
             </Jumbotron>
             <Container>
                 <Row>
-                    {Cartoon.map(item => (
+                    {this.state.cartoons.map(item => (
                         <Col size="md-3 sm-4" key={item.id}>
                             <CartoonCard 
                             onClick={this.clickedImage}
@@ -44,7 +57,6 @@ class Game extends Component {
                             />
                              
                         </Col>
-
                     ))}
                 </Row>
             </Container>
