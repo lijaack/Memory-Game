5ba6d5e510d5b8a98e0c2a6e6b775b409b188fa4
Jack <lijaack@yahoo.com>
2019-01-25 22:54:19 -0800

    initial packages

---
+++ src/pages/Game.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Nav from "../components/Nav";
+import Jumbotron from "../components/Jumbotron";
+import { Col, Row, Container } from "../components/Grid";
+
+
+class Game extends Component {
+
+    render(){
+        return(
+            <Nav/>
+            
+        
+        )
+    }
+   
+}
+
+export default Game;
