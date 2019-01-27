6c0293db4d3688b630562edca582d2813d00edf7
Jack <lijaack@yahoo.com>
2019-01-26 22:12:21 -0800

    Updates

---
+++ precache-manifest.6048f3162e924582f2b0047c5c57b9bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "b75f7c8bd582f78494df",
+    "url": "/memory-game/static/js/main.b75f7c8b.chunk.js"
+  },
+  {
+    "revision": "e8ab8b281531361a3acd",
+    "url": "/memory-game/static/js/1.e8ab8b28.chunk.js"
+  },
+  {
+    "revision": "b75f7c8bd582f78494df",
+    "url": "/memory-game/static/css/main.d03d03b6.chunk.css"
+  },
+  {
+    "revision": "c4d94b77ba4c8bb14e62e29357c84e99",
+    "url": "/memory-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e073f1e4f8c9ab930b4b8ebd723236fc.js"
+  "/memory-game/precache-manifest.6048f3162e924582f2b0047c5c57b9bc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/memory-game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
